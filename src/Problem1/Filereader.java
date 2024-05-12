94f20d119cccf111830d5d691b2c8747031e87ac
Muhannad Wahbeh <muhannadw2005@gmail.com>
2024-05-12 11:01:03 +0300

    format

--- src/Problem1/Filereader.java
+++ src/Problem1/Filereader.java
@@ -1,19 +1,17 @@
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Filereader  {
-    public String stringreader(String path){
-            // Creating a path choosing file from local
-            // directory by creating an object of Path class
-            Path fileName
-                = Path.of(path);
-     
-            // Now calling Files.readString() method to
-            // read the file
-            String str = Files.readString(fileName);
-     
-            return str;
-    }
-        
+public class Filereader {
+    public String stringreader(String path) {
+        // Creating a path choosing file from local
+        // directory by creating an object of Path class
+        Path fileName = Path.of(path);
+
+        // Now calling Files.readString() method to
+        // read the file
+        String str = Files.readString(fileName);
+
+        return str;
     }
 
+}
