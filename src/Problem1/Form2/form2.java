695a5412d9f716ca2d3bf9a8b114fc64bc4c0e25
Muhannad Wahbeh <muhannadw2005@gmail.com>
2024-05-12 12:10:14 +0300

    ll

--- src/Problem1/Form2/form2.java
+++ src/Problem1/Form2/form2.java
@@ -1,10 +1,15 @@
 package Problem1.Form2;
 
+import java.util.ArrayList;
+
 import Utils.Filereader;
 
 public class form2 {
     public static void main(String[] args) {
         String s = Filereader.stringreader("src/Problem1/Form2/test.txt");
         System.out.println(s);
+    }
+    public void importString(ArrayList<String> in) {
+        
     }
 }
