9057f0e1042d3b72a0368df4eb5e041ca9818265
Muhannad Wahbeh <muhannadw2005@gmail.com>
2024-05-12 13:09:11 +0300

    form2.java add

--- src/Problem1/Form2/form2.java
+++ src/Problem1/Form2/form2.java
@@ -7,9 +7,23 @@ import Utils.Filereader;
 public class form2 {
     public static void main(String[] args) {
         String s = Filereader.stringreader("src/Problem1/Form2/test.txt");
-        System.out.println(s);
+        int k = 0;
+        ArrayList<String> a = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '\n') {
+                a.add(s.substring(k, i-1));
+                k = i;
+            }
+        }
+        System.out.println(a);
+        for (String st : a){
+            if (!st.contains(".")) {
+                System.out.println(st);
+            }
+        }
     }
+
     public void importString(ArrayList<String> in) {
-        
+
     }
 }
