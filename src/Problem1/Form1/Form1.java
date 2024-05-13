c555fdf62132897b1bcb35579b1e565f5ab3a5ea
Ahmad Sewry <ahmadsewry@gmail.com>
2024-05-13 10:51:34 +0300

    fixed the trash of sauod

--- src/Problem1/Form1/Form1.java
+++ src/Problem1/Form1/Form1.java
@@ -12,9 +12,9 @@ public class Form1 {
      * @return
      */
     public static Node Import(String s) {
-        if(s.charAt(s.length()-1)==']') {
-            //TODO
-            return new Node(new Data(s.charAt(0),parseInteger(s).get(0),parseInteger(s).get(1)));
+        if (s.charAt(s.length() - 1) == ']') {
+            // TODO
+            return new Node(new Data(s.charAt(0), parseInteger(s).get(0), parseInteger(s).get(1)));
         }
         int cnt = 0;
         int pos = 0;
@@ -36,53 +36,52 @@ public class Form1 {
         String sr = s.substring(pos + 2, s.length() - 1);
         Node l = Import(sl);
         Node r = Import(sr);
-        //TODO
-        if(c=="|"){
-            Data data = new Data(c,l.data.length,l.data.width+r.data.width);
-
-        }else {
-        Data data = new Data(c,data.length+r.data.length,data.width);
+        // TODO
+        Data data;
+        if (c == '|') {
+            data = new Data(c, l.data.length, l.data.width + r.data.width);
+        } else {
+            data = new Data(c, l.data.length + r.data.length, l.data.width);
         }
         Node n = new Node(l, r, data);
         return n;
     }
+
     public static String Export(Node root) {
         // TODO
         if (root == null) {
             return null;
         }
-        if (root.left != null){
-            return Export(root.left);}
+        if (root.left != null) {
+            return Export(root.left);
+        }
 
-        if (root.right != null){
+        if (root.right != null) {
             return Export(root.right);
         }
-        return root.data.printData();
-            
-        
+        return root.data.toString();
+
     }
-
-
 
     public static ArrayList<Integer> parseInteger(String input) {
         ArrayList<Integer> para = new ArrayList<>();
         Pattern pattern = Pattern.compile("\\[(\\d+),");
         Matcher matcher = pattern.matcher(input);
-        
+
         if (matcher.find()) {
-             para.add(Integer.parseInt(matcher.group(1)));
+            para.add(Integer.parseInt(matcher.group(1)));
         }
-        
+
         Pattern pattern2 = Pattern.compile(",(\\d+)\\]");
         Matcher matcher2 = pattern2.matcher(input);
-        
+
         if (matcher2.find()) {
-             para.add(Integer.parseInt(matcher2.group(1)));
+            para.add(Integer.parseInt(matcher2.group(1)));
         }
-        
+
         return para;
     }
-    
+
     public static void main(String[] args) throws Exception {
         String s = Filereader.stringreader("src/Problem1/Form1/test.txt");
         System.out.println(s);
--- src/Problem1/Tree/Node.java
+++ src/Problem1/Tree/Node.java
@@ -1,15 +1,17 @@
 package Problem1.Tree;
 
 public class Node {
-    Node left, right;
-    Data data;
+    public Node left;
+    public Node right;
+    public Data data;
+
     public Node(Node left, Node right, Data data) {
         this.left = left;
         this.right = right;
         this.data = data;
     }
 
-    public Node(Data data){
+    public Node(Data data) {
         this.data = data;
     }
 
