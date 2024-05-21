bf03dba203f60b809b6061785d3ddc592b014819
Muhannad Wahbeh <muhannadw2005@gmail.com>
2024-05-21 21:04:25 +0300

    improvements

--- src/Utils/PrintingArrayList.java
+++ src/Utils/PrintingArrayList.java
@@ -3,16 +3,23 @@ package Utils;
 import java.util.ArrayList;
 
 public class PrintingArrayList {
-    public static void printCharArrayArray(ArrayList<ArrayList<Character>> array) {
-        for (ArrayList<Character> ac : array) {
-            printCharArray(ac);
-        }
-    }
-
     public static void printCharArray(ArrayList<Character> array) {
         for (char c : array) {
             System.out.print(c);
         }
         System.out.println();
     }
+
+    public static void printCGrid(ArrayList<ArrayList<Character>> array) {
+        for (ArrayList<Character> ac : array) {
+            printCharArray(ac);
+        }
+    }
+
+    public static void printCLgrid(ArrayList<ArrayList<ArrayList<Character>>> array) {
+        for (ArrayList<ArrayList<Character>> ac : array) {
+            printCGrid(ac);
+        }
+    }
+    
 }
