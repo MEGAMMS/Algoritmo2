bf69309080b3cf797f762de5548bdd11733f21b6
Mouaaz Sweid <mmsweid@gmail.com>
2024-05-31 14:44:45 +0300

    added subProblems4

---
+++ src/Problem1/SubProblem4/SubProblem4.java
@@ -0,0 +1,12 @@
+package Problem1.SubProblem4;
+
+import java.util.ArrayList;
+
+import Problem1.Tree.Node;
+
+public class SubProblem4 {
+    public static int countValidTrees(ArrayList<Node> nodes){
+
+        return 0;
+    }
+}
