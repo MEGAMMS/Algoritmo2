bdbdd60b7045744cf54a1fece92053591c448637
Ken <ahmadsoud63@gmail.com>
2024-06-05 13:55:36 +0300

    4th subproblem of problem 2 is finalized. I also created a file for tests. but did not put enough tests yet. it is up to you g3mouz

---
+++ src/Problem2/Problem2Tests.java
@@ -0,0 +1,22 @@
+package Problem2;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import Problem2.Tree.Node;
+import Utils.Filereader;
+
+public class Problem2Tests {
+    
+        @Test
+    public void test1() {
+        String in, out;
+        Node tree;
+        in = Filereader.stringreader("src/Problem2/test.txt");
+        tree = Problem2.Import(in);
+        out = Problem2.Export(tree);
+        assertEquals(out, in);
+    }
+
+}
